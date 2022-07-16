d386f4cab0dc921fa7404cca2f7942743077d396
lauren debes <laurendebes@gmail.com>
2022-07-16 17:26:13 -0500

    adding code

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,2 +1,3 @@
 mecha_df <- read.csv('MechaCar_mpg.csv', check.names = F, stringsAsFactors= F)
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mecha_df)
 summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_df))
